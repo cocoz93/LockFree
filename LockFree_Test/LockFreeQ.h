3b0461ed3ffa63e0bdf2fc3984259ea63cc325db
cocoz93 <wndnwls7@gmail.com>
2026-04-16 18:36:21 +0900

    현재진행상황 : LockFree pool,stack, queue push 구현완료 TODO : 테스트 코드 작성, 테스트, 문서(노션) 정리

---
+++ LockFree_Test/LockFreeQ.h
@@ -0,0 +1,365 @@
+﻿#pragma once
+
+
+#ifndef ____LOCKFREE_QUEUE_H____
+#define ____LOCKFREE_QUEUE_H____
+
+#include "PoolFreeList.h"
+
+namespace LockFree
+{
+
+template<typename T, bool PlacementNew = false>
+class CLockFreeQ
+{
+	//-----------------------------------------------------
+	struct NODE
+	{
+		NODE* pNextNode;
+		T Data;
+	};
+
+	struct TopNODE
+	{
+		NODE* pNode;
+		INT64 UniqueCount;
+	};
+	//-----------------------------------------------------
+
+
+
+public:
+	//최초 더미생성
+	explicit CLockFreeQ()
+	{
+		_pFreeList = nullptr;
+		_phead = nullptr;
+		_ptail = nullptr;
+		_Initialized = false;
+		_UseSize = 0;
+		_HeadUniqueCount = 0;
+		_TailUniqueCount = 0;
+
+		Init();
+	}
+
+	CLockFreeQ(const CLockFreeQ&) = delete;
+	CLockFreeQ& operator=(const CLockFreeQ&) = delete;
+	CLockFreeQ(CLockFreeQ&&) = delete;
+	CLockFreeQ& operator=(CLockFreeQ&&) = delete;
+
+	bool Init()
+	{
+		if (_Initialized)
+			return true;
+
+		_pFreeList = new(std::nothrow) CFreeList<NODE, PlacementNew>();
+		if (_pFreeList == nullptr)
+			return false;
+
+		_phead = (TopNODE*)_aligned_malloc(64, 64);
+		if (_phead == nullptr)
+		{
+			delete _pFreeList;
+			_pFreeList = nullptr;
+			return false;
+		}
+
+		_ptail = (TopNODE*)_aligned_malloc(64, 64);
+		if (_ptail == nullptr)
+		{
+			_aligned_free((void*)_phead);
+			_phead = nullptr;
+			delete _pFreeList;
+			_pFreeList = nullptr;
+			return false;
+		}
+
+		NODE* pDummy = _pFreeList->Alloc();
+		if (pDummy == nullptr)
+		{
+			_aligned_free((void*)_ptail);
+			_ptail = nullptr;
+			_aligned_free((void*)_phead);
+			_phead = nullptr;
+			delete _pFreeList;
+			_pFreeList = nullptr;
+			return false;
+		}
+		pDummy->pNextNode = nullptr;
+
+		_phead->pNode = pDummy;
+		_phead->UniqueCount = 0;
+
+		_ptail->pNode = pDummy;
+		_ptail->UniqueCount = 0;
+
+		_UseSize = 0;
+		_HeadUniqueCount = 0;
+		_TailUniqueCount = 0;
+
+		_Initialized = true;
+		return true;
+	}
+
+	~CLockFreeQ()
+	{
+		if (_Initialized == false)
+			return;
+
+		Clear();
+
+		_pFreeList->Free(this->_phead->pNode);
+
+		_aligned_free((void*)this->_ptail);
+		_aligned_free((void*)this->_phead);
+
+		delete _pFreeList;
+	}
+
+	void Clear(void)
+	{
+		if (_Initialized == false)
+			return;
+
+		//모든 노드 삭제
+		NODE* pfNode = nullptr;
+
+		while (this->_phead->pNode->pNextNode != nullptr)
+		{
+			pfNode = this->_phead->pNode->pNextNode;
+			this->_phead->pNode->pNextNode = this->_phead->pNode->pNextNode->pNextNode;
+			_pFreeList->Free(pfNode);
+		}
+
+		_phead->UniqueCount = 0;
+		_ptail->UniqueCount = 0;
+		_ptail->pNode = _phead->pNode;
+
+		_UseSize = 0;
+		_HeadUniqueCount = 0;
+		_TailUniqueCount = 0;
+	}
+
+	// 락프리 특성상 정확한 사이즈 보장 불가 (스냅샷 힌트)
+	bool IsEmpty(void)
+	{
+		return (_UseSize == 0);
+	}
+
+
+	bool Enqueue(T Data)
+	{
+		TopNODE bTopTailNode;						// backup TailTopNode;
+		NODE* pbTailNextNode;						// backupTailNext Node;
+		NODE* pnNode = this->_pFreeList->Alloc();	// NewNode;
+
+		pnNode->Data = Data;
+		pnNode->pNextNode = nullptr;				// Enqueue는 pNext가 nullptr일 경우에만 
+
+		INT64 lTailUniqueCount = InterlockedIncrement64(&this->_TailUniqueCount);
+		int backoff = 1;
+
+		// 노드가 추가되면 Enqueue성공 간주. tail밀기 실패는 상관X
+		while (true)
+		{
+			// tail백업
+			bTopTailNode.UniqueCount = this->_ptail->UniqueCount;
+			bTopTailNode.pNode = this->_ptail->pNode;
+
+			// stale 감지 시 역참조+CAS 회피 (~20-40 cycles 절감)
+			if (bTopTailNode.UniqueCount != this->_ptail->UniqueCount)
+				continue;
+
+			_mm_prefetch((const char*)bTopTailNode.pNode, _MM_HINT_T0);
+
+			//tail의 Next백업
+			pbTailNextNode = bTopTailNode.pNode->pNextNode;
+
+			//_______________________________________________________________________________________
+			// 
+			//	tail뒤에 노드가 존재하는 경우 - 밀어준다.
+			//_______________________________________________________________________________________
+			if (nullptr != pbTailNextNode)
+			{
+				lTailUniqueCount = InterlockedIncrement64(&this->_TailUniqueCount);
+
+				InterlockedCompareExchange128
+				(
+					(volatile INT64*)_ptail,
+					(INT64)lTailUniqueCount,
+					(INT64)pbTailNextNode,
+					(INT64*)&bTopTailNode
+				);
+				continue;
+			}
+			//_______________________________________________________________________________________
+
+			//_______________________________________________________________________________________
+			// 
+			//	Enqueue시도 (CAS)
+			//_______________________________________________________________________________________
+			else
+			{
+				if (nullptr == InterlockedCompareExchangePointer
+				(
+					(volatile PVOID*)&bTopTailNode.pNode->pNextNode,
+					(PVOID)pnNode,
+					(PVOID)pbTailNextNode
+				))
+				{
+					// Enqueue 성공 
+					// tail 밀어준다 (성공여부 판단x)
+					InterlockedCompareExchange128
+					(
+						(volatile INT64*)_ptail,
+						(INT64)lTailUniqueCount,
+						(INT64)pnNode,
+						(INT64*)&bTopTailNode
+					);
+					break;
+				}
+
+				for (int i = 0; i < backoff; ++i)
+					YieldProcessor();
+				if (backoff < 16)
+					backoff <<= 1;
+			}
+			//_______________________________________________________________________________________
+		}
+
+		InterlockedIncrement64(&this->_UseSize);
+		return true;
+	}
+
+
+	bool Dequeue(T* pOutData)
+	{
+		INT64 lHeadUniqueCount = InterlockedIncrement64(&this->_HeadUniqueCount);
+		INT64 lTailUniqueCount;
+
+		TopNODE	 bTopHeadNode;
+		TopNODE	 bTopTailNode;
+		NODE* pbTailNextNode;
+		NODE* bHeadNextNode;
+		int backoff = 1;
+
+		while (true)
+		{
+			//_______________________________________________________________________________________
+			// 
+			//	tail뒤에 노드가 존재하는 경우 - 밀어준다.
+			//_______________________________________________________________________________________
+
+			// tail백업
+			bTopTailNode.UniqueCount = this->_ptail->UniqueCount;
+			bTopTailNode.pNode = this->_ptail->pNode;
+
+			// stale 감지 시 역참조+CAS 회피 (~20-40 cycles 절감)
+			if (bTopTailNode.UniqueCount != this->_ptail->UniqueCount)
+				continue;
+
+			_mm_prefetch((const char*)bTopTailNode.pNode, _MM_HINT_T0);
+
+			//tail의 Next백업
+			pbTailNextNode = bTopTailNode.pNode->pNextNode;
+
+			if (nullptr != pbTailNextNode)
+			{
+				lTailUniqueCount = InterlockedIncrement64((volatile INT64*)&this->_TailUniqueCount);
+
+				InterlockedCompareExchange128
+				(
+					(volatile INT64*)_ptail,
+					(INT64)lTailUniqueCount,
+					(INT64)pbTailNextNode,
+					(INT64*)&bTopTailNode
+				);
+				continue;
+			}
+			//_______________________________________________________________________________________
+
+
+			//_______________________________________________________________________________________
+			// 
+			//	Dequeue
+			//_______________________________________________________________________________________
+
+			// head 백업
+			bTopHeadNode.UniqueCount = this->_phead->UniqueCount;
+			bTopHeadNode.pNode = this->_phead->pNode;
+
+			// stale 감지 시 역참조+CAS 회피 (~20-40 cycles 절감)
+			if (bTopHeadNode.UniqueCount != this->_phead->UniqueCount)
+				continue;
+
+			_mm_prefetch((const char*)bTopHeadNode.pNode, _MM_HINT_T0);
+
+			bHeadNextNode = bTopHeadNode.pNode->pNextNode;
+
+			// 큐가 비어있으면 실패 (호출자 책임)
+			if (bHeadNextNode == nullptr)
+				return false;
+
+			// head==tail: Enqueue 직후 tail이 안 밀린 상태 — tail push 후 재시도 (댕글링 방지)
+			if (bTopHeadNode.pNode == bTopTailNode.pNode)
+			{
+				lTailUniqueCount = InterlockedIncrement64((volatile INT64*)&this->_TailUniqueCount);
+
+				InterlockedCompareExchange128
+				(
+					(volatile INT64*)_ptail,
+					(INT64)lTailUniqueCount,
+					(INT64)bHeadNextNode,
+					(INT64*)&bTopTailNode
+				);
+				continue;
+			}
+
+			_mm_prefetch((const char*)bHeadNextNode, _MM_HINT_T0);
+
+			*pOutData = bHeadNextNode->Data;
+
+			if (false == InterlockedCompareExchange128
+			(
+				(volatile INT64*)this->_phead,
+				(INT64)lHeadUniqueCount,
+				(INT64)bHeadNextNode,
+				(INT64*)&bTopHeadNode
+			))
+			{
+				// DCAS 실패
+				for (int i = 0; i < backoff; ++i)
+					YieldProcessor();
+				if (backoff < 16)
+					backoff <<= 1;
+				continue;
+			}
+			else
+			{
+				// DCAS 성공
+				break;
+			}
+			//____________________________________________________________________
+		}
+
+		// CAS128()가 Comp쪽으로 뱉어준 원래노드를 해제
+		this->_pFreeList->Free(bTopHeadNode.pNode);
+		InterlockedDecrement64(&this->_UseSize);
+
+		return true;
+	}
+private:
+	CFreeList<NODE, PlacementNew>* _pFreeList;
+	volatile TopNODE* _phead;
+	volatile TopNODE* _ptail;
+	bool _Initialized;
+	alignas(64) volatile INT64	_UseSize;
+	alignas(64) volatile INT64 _HeadUniqueCount;
+	alignas(64) volatile INT64 _TailUniqueCount;
+};
+
+}
+
+
+#endif
---
+++ LockFree_Test/PoolFreeList.h
@@ -0,0 +1,281 @@
+﻿#pragma once
+
+
+#ifndef ____POOL_FREE_LIST_H____
+#define ____POOL_FREE_LIST_H____
+
+
+#include <windows.h>
+#include <new>
+#include <intrin.h>
+
+#define IDENT_VAL 0x6659
+
+namespace LockFree
+{
+	namespace Internal
+	{
+
+		template<typename T, bool PlacementNew = false>
+		class CPoolFreeList
+		{
+
+			struct NODE
+			{
+				NODE* pNextNode;
+				T		Data;
+			};
+
+			__forceinline static NODE* DataToNode(T* data)
+			{
+				return reinterpret_cast<NODE*>(
+					reinterpret_cast<char*>(data) - offsetof(NODE, Data));
+			}
+
+			__forceinline static T* NodeToData(NODE* node)
+			{
+				return &node->Data;
+			}
+
+			struct TopNODE
+			{
+				NODE* volatile pNode;
+				volatile INT64 UniqueCount;
+			};
+
+			// Intel oneTBB atomic_backoff 방식: Spin(pause 지수증가) → Yield(SwitchToThread)
+			struct CASBackoff
+			{
+				static constexpr int LOOPS_BEFORE_YIELD = 32;
+				int _count = 1;
+
+				__forceinline void Pause()
+				{
+					if (_count <= LOOPS_BEFORE_YIELD)
+					{
+						for (int i = 0; i < _count; ++i)
+							YieldProcessor();
+						_count <<= 1;
+					}
+					else
+					{
+						SwitchToThread();
+					}
+				}
+			};
+
+
+		public:
+			explicit CPoolFreeList()
+			{
+				this->_pTopNode = nullptr;
+				hHeap = nullptr;
+				this->_Initialized = false;
+				this->_AllocCount = 0;
+
+				Init();
+			}
+
+			CPoolFreeList(const CPoolFreeList&) = delete;
+			CPoolFreeList& operator=(const CPoolFreeList&) = delete;
+			CPoolFreeList(CPoolFreeList&&) = delete;
+			CPoolFreeList& operator=(CPoolFreeList&&) = delete;
+
+			bool Init()
+			{
+				if (this->_Initialized)
+					return true;
+
+				this->_pTopNode = (TopNODE*)_aligned_malloc(64, 64);
+				if (this->_pTopNode == nullptr)
+					return false;
+
+				this->_pTopNode->pNode = nullptr;
+				this->_pTopNode->UniqueCount = 0;
+
+				hHeap = HeapCreate(NULL, 0, NULL);
+				if (hHeap == nullptr)
+				{
+					_aligned_free((void*)this->_pTopNode);
+					this->_pTopNode = nullptr;
+					return false;
+				}
+
+				// 저단편화 힙(LFH) 설정
+				ULONG HeapInformationValue = 2;
+				HeapSetInformation(hHeap, HeapCompatibilityInformation,
+					&HeapInformationValue, sizeof(HeapInformationValue));
+
+				this->_Initialized = true;
+				return true;
+			}
+
+			~CPoolFreeList()
+			{
+				if (this->_Initialized == false)
+					return;
+
+				NODE* pfNode = nullptr;		//DeleteNode
+
+				while (this->_pTopNode->pNode != nullptr)
+				{
+					pfNode = this->_pTopNode->pNode;
+					this->_pTopNode->pNode = this->_pTopNode->pNode->pNextNode;
+					// PlacementNew 모드: free list의 노드는 이미 Free()에서 소멸자 호출됨
+					if constexpr (!PlacementNew)
+						pfNode->Data.~T();
+
+					//delete pfNode;
+					HeapFree(hHeap, 0, pfNode);	// HeapFree
+				}
+
+				_aligned_free((void*)this->_pTopNode);
+				HeapDestroy(hHeap);
+			}
+
+		public:
+			bool Free(T* Data)
+			{
+				if (this->_Initialized == false)
+					return false;
+
+				if (Data == nullptr)
+					return false;
+
+				// Free Node
+				NODE* fNode = DataToNode(Data);
+
+				// 잘못된 주소가 전달된 경우
+				//if (fNode->IsMine != IDENT_VAL)
+					//return false;
+
+				// 소멸자 호출 (free list 반환 전에 호출해야 use-after-free 방지)
+				if constexpr (PlacementNew)
+					fNode->Data.~T();
+
+				// backup TopNode
+				TopNODE bTopNode;
+				CASBackoff backoff;
+
+				//_______________________________________________________________________________________
+				//  
+				//	CAS Version
+				//_______________________________________________________________________________________
+				while (true)
+				{
+					bTopNode.pNode = this->_pTopNode->pNode;
+					fNode->pNextNode = bTopNode.pNode;
+
+					NODE* pNode = (NODE*)InterlockedCompareExchangePointer
+					(
+						(volatile PVOID*)&this->_pTopNode->pNode,
+						(PVOID)fNode,
+						(PVOID)bTopNode.pNode
+					);
+
+					if (pNode != bTopNode.pNode)
+					{
+						backoff.Pause();
+						continue;
+					}
+					else
+					{
+						break;
+					}
+				}
+				//_______________________________________________________________________________________
+
+				return true;
+			}
+
+
+			__declspec(noinline) T* AllocNewNode()
+			{
+				if (this->_Initialized == false)
+					return nullptr;
+
+				// TODO : 더 최적화하고자 한다면 virtualAlloc
+				NODE* rNode = (NODE*)HeapAlloc(this->hHeap, FALSE, sizeof(NODE));
+				if (rNode == nullptr)
+					return nullptr;
+
+				new(&rNode->Data) T;
+				rNode->pNextNode = nullptr;
+				InterlockedIncrement64(&this->_AllocCount);
+				return NodeToData(rNode);
+			}
+
+			T* Alloc()
+			{
+				if (this->_Initialized == false)
+					return nullptr;
+
+				TopNODE bTopNode;
+				CASBackoff backoff;
+
+				// free list에서 pop 시도 (hot path)
+				while (true)
+				{
+					bTopNode.UniqueCount = this->_pTopNode->UniqueCount;
+					bTopNode.pNode = this->_pTopNode->pNode;
+
+					// free list가 비어있으면 새 노드 할당 (cold path)
+					if (bTopNode.pNode == nullptr)
+						return AllocNewNode();
+
+					_mm_prefetch((const char*)bTopNode.pNode, _MM_HINT_T0);
+
+					//CAS를 덜 호출하기위함
+					if (bTopNode.UniqueCount != this->_pTopNode->UniqueCount)
+						continue;
+
+					if (false == InterlockedCompareExchange128
+					(
+						(volatile INT64*)this->_pTopNode,
+						(INT64)(bTopNode.UniqueCount + 1),
+						(INT64)bTopNode.pNode->pNextNode,
+						(INT64*)&bTopNode
+					))
+					{
+						//CAS 실패
+						backoff.Pause();
+						continue;
+					}
+					else
+					{
+						//CAS 성공
+						break;
+					}
+				}
+
+				NODE* rNode = bTopNode.pNode;
+
+				// 생성자 호출
+				if constexpr (PlacementNew)
+					new (&rNode->Data) T;
+
+				return NodeToData(rNode);
+			}
+		public:
+			// 총 HeapAlloc된 노드 수 (단조 증가, cold path에서만 갱신)
+			INT64 GetAllocCount() const { return _AllocCount; }
+
+		private:
+			TopNODE* _pTopNode;		//_allinge_malloc()
+			HANDLE hHeap;
+			bool _Initialized;
+
+			alignas(64) volatile INT64	_AllocCount;	// HeapAlloc된 전체 노드 수
+		};
+
+	}
+
+	template<typename T, bool PlacementNew = false>
+	using CPoolFreeList = Internal::CPoolFreeList<T, PlacementNew>;
+
+	template<typename T, bool PlacementNew = false>
+	using CFreeList = CPoolFreeList<T, PlacementNew>;
+
+}
+
+#endif //____POOL_FREE_LIST_H____
