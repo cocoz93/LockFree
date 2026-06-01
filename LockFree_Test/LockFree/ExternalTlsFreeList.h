c23f16efa0952764bce6107f0fbbfa361ff0beb8
cocoz93 <wndnwls7@gmail.com>
2026-06-02 01:12:07 +0900

    레거시 제거 + 워밍업 파라미터화   - 항상 켜진 Free 검증 → 릴리즈 fail-fast + 디버그 assert로 전환   - 워밍업 청크 수를 생성자 인자(warmupChunkCount)로 파라미터화   - [결함 E] 추적 태그 제거, 의도 주석으로 대체   - 죽은 레거시 블록/include/별칭 제거

--- LockFree_Test/LockFree/ExternalTlsFreeList.h
+++ LockFree_Test/LockFree/ExternalTlsFreeList.h
@@ -4,7 +4,7 @@
 #define ____EXTERNAL_TLS_FREE_LIST_H____
 
 #include "InternalFreeList.h"
-//#include "SListFreeList.h"
+#include <cassert>
 
 namespace LockFree
 {
@@ -49,8 +49,8 @@ public:
 	public:
 		void Initialize()
 		{
-			//청크 클래스 구분
-			this->Config = InterlockedIncrement64(&g_Config);	// [결함 E] 원자적 증가
+			// 청크마다 유일한 식별자 부여. 멀티스레드 동시 생성 가능하므로 원자적 증가.
+			this->Config = InterlockedIncrement64(&g_Config);
 
 			for (int i = 0; i < CHUNK_SIZE; ++i)
 			{
@@ -75,18 +75,20 @@ public:
 
 
 public:
-	explicit CExternalTlsFreeList(bool IsPlacementNew = false)
+	// warmupChunkCount: 시작 시 미리 만들어둘 청크 수. 보통 동시 사용 스레드 수만큼 주면
+	//                   각 스레드의 첫 Alloc이 HeapAlloc 없이 처리된다. 0이면 워밍업 생략(lazy).
+	explicit CExternalTlsFreeList(bool IsPlacementNew = false, int warmupChunkCount = 16)
 	{
 		// ChunkNode의 생성 및 생성자 여부결정
 		// Config와 this(찾아갈주소)는 한번 박아놓으면 바뀔일 없으므로 PlacementNew = false (기본값)
 		this->_ChunkFreeList = nullptr;
-		//this->_ChunkFreeList = new CSListFreeList<ChunkNODE>;
 
 		// <T>자료형 자체에 대한 생성자 호출 여부
 		this->_IsPlacementNew = IsPlacementNew;
 
 		this->TlsIndex = TLS_OUT_OF_INDEXES;
 		this->_Initialized = false;
+		this->_WarmupChunkCount = warmupChunkCount;
 
 		Init();
 	}
@@ -112,14 +114,21 @@ public:
 		if (this->TlsIndex == TLS_OUT_OF_INDEXES)
 			return false;
 
-		// 저단편화를 막아보자
-		ChunkNODE* pChunkNode[16];
+		// 워밍업: 청크 N개를 미리 free list에 적재 (일괄 Alloc 후 일괄 Free해야 서로 다른 N개가 쌓임)
+		if (this->_WarmupChunkCount > 0)
+		{
+			ChunkNODE** pWarmup = new(std::nothrow) ChunkNODE*[this->_WarmupChunkCount];
+			if (pWarmup != nullptr)
+			{
+				for (int i = 0; i < this->_WarmupChunkCount; ++i)
+					pWarmup[i] = this->_ChunkFreeList->Alloc();
 
-		for (int i = 0; i < _countof(pChunkNode); ++i)
-			pChunkNode[i] = this->_ChunkFreeList->Alloc();
+				for (int i = 0; i < this->_WarmupChunkCount; ++i)
+					this->_ChunkFreeList->Free(pWarmup[i]);	// nullptr는 Free 내부에서 무시됨
 
-		for (int i = 0; i < _countof(pChunkNode); ++i)
-			this->_ChunkFreeList->Free(pChunkNode[i]);
+				delete[] pWarmup;
+			}
+		}
 
 		this->_Initialized = true;
 		return true;
@@ -180,11 +189,9 @@ public:
 
 	bool Free(volatile T* Data)
 	{
-		// 내 메모리풀에서 나간게 맞는가?
-		if (((ChunkDATA*)Data)->DataConfig != (((ChunkNODE*)((ChunkDATA*)Data)->pMyChunkNode)->Config))
-		{
-			return false;
-		}
+		// 릴리즈는 fail-fast(이 풀의 유효 포인터 전제, delete와 동일).
+		// 디버그에서만 슬롯 자기일관성(DataConfig == 청크 Config)을 assert로 탐지.
+		assert(((ChunkDATA*)Data)->DataConfig == ((ChunkDATA*)Data)->pMyChunkNode->Config && "오염되었거나 이 풀의 슬롯이 아님");
 
 		ChunkNODE* pChunkNode = ((ChunkDATA*)Data)->pMyChunkNode;
 
@@ -208,211 +215,9 @@ private:
 	int TlsIndex;
 	bool _IsPlacementNew;		// 데이터 <T>에 대한 생성자 여부결정
 	bool _Initialized;
+	int _WarmupChunkCount;		// 시작 시 미리 만들어둘 청크 수 (보통 동시 사용 스레드 수)
 };
-
-template<typename T>
-using CExternalTLS_LockFree_FreeList = CExternalTlsFreeList<T>;
-
-template<typename T>
-using CTlsFreeList = CExternalTlsFreeList<T>;
-
-template<typename T>
-using CTLS_LockFree_FreeList = CExternalTlsFreeList<T>;
 
 }
 
 #endif //____EXTERNAL_TLS_FREE_LIST_H____
-
-
-
-
-// 
-//===================================================================================
-// 최적화 이전버전
-//===================================================================================
-//#pragma once
-//
-//
-//#ifndef ____TLS_LOCKFREE_FREELIST_H____
-//#define ____TLS_LOCKFREE_FREELIST_H____
-//
-//#include "LockFree_FreeList.h"
-//#include "LockFreeStack.h"
-//#include <map>
-//
-//#define CHUNK_SIZE 1000
-//
-//static LONG64 g_Config = 0x6656;
-//
-////이거지금 SendFlag하듯이 해서 오류가 나는지 확인해보기
-//
-//template<typename T>
-//class CTLS_LockFree_FreeList
-//{
-//public:
-//	struct ChunkNODE;
-//
-//	struct ChunkDATA
-//	{
-//		T Data;
-//		ChunkNODE* pMyChunkNode;
-//		LONG64 DataConfig;
-//	};
-//
-//	struct ChunkNODE
-//	{
-//	public:
-//		ChunkDATA DataArr[CHUNK_SIZE];
-//		alignas(64) long long AllocCount;
-//		alignas(64) long long FreeCount;
-//		DWORD ThreadId;
-//		bool UseFlag;
-//	};
-//
-//
-//public:
-//	explicit CTLS_LockFree_FreeList()
-//	{
-//		this->Config = ++g_Config;
-//
-//		this->TlsIndex = TlsAlloc();
-//		if (TlsIndex == TLS_OUT_OF_INDEXES)
-//		{
-//			int* a = nullptr;
-//			*a = 0;
-//		}
-//	}	
-//	virtual ~CTLS_LockFree_FreeList() {}
-//
-//
-//public:
-//	//DataAlloc
-//	T* Alloc()
-//	{
-//		//Tls->Map Debug
-//		ChunkNODE* pChunkNode = (ChunkNODE*)TlsGetValue(this->TlsIndex);
-//
-//		//해당스레드에서 Tls가 최초 호출된 경우
-//		if (pChunkNode == nullptr)
-//		{
-//			//TlsGetValue 에러검사
-//			if (pChunkNode == ERROR_SUCCESS)
-//			{
-//				if (0 != GetLastError())
-//				{
-//					int* a = nullptr;
-//					*a = 0;
-//				}
-//			}
-//
-//			ChunkAlloc(&pChunkNode);
-//			return &(pChunkNode->DataArr[0].Data);
-//		}
-//
-//		//이미 Tls가 있는 경우
-//		else if (pChunkNode != nullptr)
-//		{
-//			//마지막인자인경우, SetValue를 해놓고 들어간다.
-//			if (pChunkNode->AllocCount == CHUNK_SIZE - 1)
-//			{
-//				++pChunkNode->AllocCount;
-//				ChunkNODE* pNewChunkNode = nullptr;
-//				ChunkAlloc(&pNewChunkNode);
-//				--pNewChunkNode->AllocCount;
-//
-//				return &(pChunkNode->DataArr[CHUNK_SIZE - 1].Data);
-//			}
-//
-//			//청크를 모두 사용한 경우, 새로운 청크 할당
-//			else if (pChunkNode->AllocCount >= CHUNK_SIZE)
-//			{
-//				ChunkNODE* pNewChunkNode = nullptr;
-//				ChunkAlloc(&pNewChunkNode);
-//				return &(pNewChunkNode->DataArr[0].Data);
-//			}
-//
-//			//청크가 남아있는 경우
-//			else if (pChunkNode->AllocCount < CHUNK_SIZE)
-//			{
-//				++pChunkNode->AllocCount;
-//				return &(pChunkNode->DataArr[pChunkNode->AllocCount - 1].Data);
-//			}
-//		}
-//	}
-//
-//	void Free(T* Data)
-//	{
-//		if (((ChunkDATA*)Data)->DataConfig != this->Config)
-//		{
-//			int* Crash = 0;
-//			*Crash = 0;
-//		}
-//
-//		//청크 Free카운트를 증가시키고, 모두 반납된경우 프리리스트로 반납한다.
-//		if (CHUNK_SIZE <= InterlockedIncrement64(&(((ChunkNODE*)((ChunkDATA*)Data)->pMyChunkNode)->FreeCount)))
-//		{
-//			//printf("[%d]\t  Free(%d/%d) \t %X\n", GetCurrentThreadId(), AllocCount, FreeCount, pChunkNode);
-//			
-//			//프리리스트 반환
-//			this->_ChunkFreeList.Free((ChunkNODE*)((ChunkDATA*)Data)->pMyChunkNode);
-//			//delete ((ChunkNODE*)((ChunkDATA*)Data)->pMyChunkNode);
-//		}
-//	}
-//
-//
-//
-//
-//public:
-//	//size단위로 메모리블럭을 확보환다.
-//	bool ChunkAlloc(ChunkNODE** ppChunkNode)
-//	{
-//		//ChunkNODE* pNewChunkNode = new ChunkNODE;
-//		ChunkNODE* pNewChunkNode = this->_ChunkFreeList.Alloc();
-//		
-//		pNewChunkNode->FreeCount = 0;
-//		pNewChunkNode->AllocCount = 1;
-//
-//		for (int i = 0; i < CHUNK_SIZE; ++i)
-//		{
-//			pNewChunkNode->DataArr[i].pMyChunkNode = pNewChunkNode;
-//			pNewChunkNode->DataArr[i].DataConfig = this->Config;
-//		}
-//
-//		if (0 == TlsSetValue(this->TlsIndex, pNewChunkNode))
-//		{
-//			int* a = nullptr;
-//			*a = 0;
-//		}
-//		
-//		*ppChunkNode = pNewChunkNode;
-//		
-//		//Deubg
-//		//printf("[%d]\t Alloc(%d/%d) \t\t %X\n", GetCurrentThreadId(), pNewChunkNode->AllocCount, pNewChunkNode->FreeCount, pNewChunkNode);
-//		return true;
-//	}
-//
-//
-//	int GetChunkSize()
-//	{
-//		return this->_ChunkFreeList.GetUseCount();
-//	}
-//
-//
-//	void DebugPrint()
-//	{
-//		printf("ChunkNODE Use / 
-//  : [ %d / %d ]\n", _ChunkFreeList.GetUseCount(), _ChunkFreeList.GetAllocCount());
-//	}
-//
-//
-//
-//	
-//private:
-//	int TlsIndex;
-//	//식별자. ASCII CMC(677767) + Count(0)
-//	CLockFree_FreeList<ChunkNODE> _ChunkFreeList;
-//	LONG64 Config;
-//};
-//
-//#endif //____TLS_LOCKFREE_FREELIST_H____
-//
